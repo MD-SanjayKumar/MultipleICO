6a87f9d859e03c8bea8c89ac9588b07ab8895590
sanjaykumar <sanjay.kumar@minddeft.net>
2023-05-03 17:34:05 +0530

    new changes

--- initial.js
+++ initial.js
@@ -3,67 +3,8 @@ const app = express();
 const path = require('path');
 const port = '8000';
 var { graphql, buildSchema } = require("graphql");
-
-
-app.get("/idea", async(req, res)=>{
-    async function fetchGraphQL(operationsDoc, operationName, variables) {
-        const result = await fetch(
-          "https://indexer-testnet.staging.gcp.aptosdev.com/v1/graphql",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              query: operationsDoc,
-              variables: variables,
-              operationName: operationName
-            })
-          }
-        );
-        return await result.json();
-      }
-      
-      const operationsDoc = `
-        query MyQuery {
-          current_table_items(where: {table_handle: {_eq: "0xd36821f9ab549a11fe742ce27da14b8d41468d32fc7ad886a79b04b2004db41c"}}) {
-            decoded_key
-            decoded_value
-          }
-        }
-      `;
-      
-      function fetchMyQuery() {
-        return fetchGraphQL(
-          operationsDoc,
-          "MyQuery",
-          {}
-        );
-      }
-      
-      async function startFetchMyQuery() {
-        const { errors, data } = await fetchMyQuery();
-        if (errors) {
-          // handle those errors like a pro
-          console.error(errors);
-        }
-      
-        // do something great with this precious data
-
-        // for (var i in data.current_table_items)
-        // {
-        //   console.log('---------');
-        //   console.log(i);
-        //   console.log(data.current_table_items[i]);
-        //   console.log('---------');
-        // }
-        for (var i in data.current_table_items){
-          console.log(data.current_table_items[i]);
-          console.log(i);
-        }
-        // console.log(data);
-        // console.log(data.current_table_items.length);
-        // console.log(data.current_table_items[0].decoded_value);
-      }
-      startFetchMyQuery();
-})
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 app.get("/", async(req, res)=>{
   res.sendFile(path.join(__dirname, "html", "home.html"));
